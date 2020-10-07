3d47a66ed0328c4cbc5840bf6c3183adae9498e5
Julián Andrés Flórez Cendales <julian.1701913963@ucaldas.edu.co>
2020-10-07 08:34:00 +0100

    videos 3-4
    se continua con la creaciÃ³n de app.js y de los controladores, se crea
    el notification.helper (para usar a futuro con Apis como twilio

--- model/dto/teacher.dto.js
+++ model/dto/teacher.dto.js
@@ -1,9 +1,10 @@
 /** packages */
 const mongoose = require("mongoose");
+const db = require("../db-connection/mongodb");
 
 /** using schema */
 const schema = require("../schemas/teacher.schema");
-
+db();
 schema.statics = {
     create: function (data, cb){
         let doc = new this(data);
